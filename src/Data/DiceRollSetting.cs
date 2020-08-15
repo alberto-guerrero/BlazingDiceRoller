39119cad5ebadc4c72387ba09d195918c174b29b
alberto_guerrero <albrt83@gmail.com>
2020-08-15 19:01:39 -0400

    Initial Commit

---
+++ src/Data/DiceRollSetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingDiceRoller.Data
+{
+    public class DiceRollSetting
+    {
+        public int MaxValue { get; set; }
+
+        public int NumberOfDice { get; set; }
+
+        public int Modifier { get; set; }
+
+        public int LastResult { get; set; }
+    }
+}
